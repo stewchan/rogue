957863ae65ff80125faef71f1f5f77b294cdf279
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-09 15:03:13 -0500

    Add enemy navigation

---
+++ characters/Enemy.gd
@@ -0,0 +1,29 @@
+class_name Enemy, "res://assets/enemies/goblin/goblin_idle_anim_f0.png"
+extends Character
+
+
+onready var navigation: Navigation2D = get_tree().current_scene.get_node("Navigation2D")
+onready var player: KinematicBody2D = get_tree().current_scene.get_node("Player")
+
+var path: PoolVector2Array
+
+
+func chase() -> void:
+	if path:
+		var vector_to_next_point: Vector2 = path[0] - global_position
+		var distance_to_next_point: float = vector_to_next_point.length()
+		if distance_to_next_point < 1:
+			path.remove(0)
+			if not path:
+				return
+		move_direction = vector_to_next_point
+		if vector_to_next_point.x > 0 and animated_sprite.flip_h:
+			animated_sprite.flip_h = false
+		elif vector_to_next_point.x < 0 and not animated_sprite.flip_h:
+			animated_sprite.flip_h = true
+
+
+func _on_PathTimer_timeout() -> void:
+	path = navigation.get_simple_path(global_position, player.global_position)
+	
+
---
+++ characters/FlyingCreatureFSM.gd
@@ -0,0 +1,26 @@
+extends FSM
+
+
+func _init() -> void:
+	_add_state("chase")
+	
+
+func _ready() -> void:
+	set_state(states.chase)
+
+
+func _state_logic(_delta: float) -> void:
+	match state:
+		states.chase:
+			parent.chase()
+			parent.move()
+
+
+func _get_transition() -> int:
+	return -1
+
+
+func _enter_state(_previous_state: int, new_state: int) -> void:
+	match new_state:
+		states.chase:
+			animation_player.play("fly")
