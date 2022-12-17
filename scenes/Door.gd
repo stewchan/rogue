726f33e6b072d031b2cef6ddf104350bef7b0e3a
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-17 17:16:33 -0500

    Door fix

--- scenes/Door.gd
+++ scenes/Door.gd
@@ -1,13 +1,9 @@
 extends StaticBody2D
 
-onready var animated_sprite: AnimatedSprite = $AnimatedSprite
+onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func open() -> void:
-	animated_sprite.play("opening")
+	animation_player.play("open")
 
-
-func _on_AnimatedSprite_animation_finished() -> void:
-	animated_sprite.play("fully_open")
-	animated_sprite.stop()
 	
--- scenes/Rooms.gd
+++ scenes/Rooms.gd
@@ -12,28 +12,34 @@ var cell_size: int = 16
 var num_rooms: int = 5
 
 func spawn_rooms() -> void:
+	randomize()
 	var prev_room: Node2D
 	var room: Node2D
 	for i in range(0, num_rooms):
+		var size = Vector2(floor(randi()%6 + 8), floor(randi()%6 + 8))
+		
 		if i == 0:
-			room = generate_room(Vector2(10,10), true)
+			room = generate_room(size, true)
 		elif i == num_rooms - 1:
-			room = 	generate_room(Vector2(10,10), false, true)
+			room = 	generate_room(size, false, true)
 		else:
-			room = 	generate_room(Vector2(10,10))
+			room = 	generate_room(size)
 		room.name = "Room" + str(i)
+		room.z_index = -i
 		if prev_room:
-			var door_x = prev_room.get_node("Doors").get_child(0).position.x
-			var entrance_x = room.get_node("Entrance").get_child(0).position.x
-			print(str(door_x) + " : " + str(entrance_x))
-			room.global_position.x += door_x - entrance_x
-			room.global_position.y -=  12 * cell_size * i
+			var door_pos = prev_room.get_node("Doors").get_child(0).global_position
+			var entrance_pos = room.get_node("Entrance").get_child(0).global_position
+			room.position.x += door_pos.x - entrance_pos.x
+			var wtm = prev_room.get_node("MainTilemap").world_to_map(prev_room.position)
+			room.position.y =  (wtm.y - size.y) * cell_size
+			print(str(prev_room.position.y) + ":" + str(wtm.y * cell_size) + ":" + str(room.position.y))
+			
+			
 		prev_room = room
 		add_child(room)
 
 	
-func generate_room(room_size: Vector2, is_start_room: bool = false, is_end_room: bool = false) -> Node2D:
-	camera.position = room_size * cell_size * 0.5
+func generate_room(room_size: Vector2, start_room: bool = false, end_room: bool = false) -> Node2D:
 	var room = room_scene.instance()
 	var main_tilemap = room.get_node("MainTilemap")
 	var bottom_tilemap = room.get_node("BottomTilemap")
@@ -62,17 +68,16 @@ func generate_room(room_size: Vector2, is_start_room: bool = false, is_end_room:
 	bottom_tilemap.set_cell(room_size.x, room_size.y-1, 6, false, false, false, Vector2(2,1)) # Bottom right
 	
 	# Generate door
-	var	door_x = int(room_size.x/4) + randi() % int(room_size.x/2)
-	var door = door_scene.instance()
-	door.position = Vector2(door_x, 0) * cell_size
-	main_tilemap.set_cell(door_x, 0, -1)
-	main_tilemap.set_cell(door_x-1, 0, -1)
-	main_tilemap.set_cell(door_x, -1, -1)
-	main_tilemap.set_cell(door_x-1, -1, -1)
-	room.get_node("Doors").add_child(door)
-	# Door trigger
-	room.get_node("DoorTrigger").position = door.position + Vector2(0, cell_size * 1.5)
-	
+	if not end_room:
+		var	door_x = int(room_size.x/4) + randi() % int(room_size.x/2)
+		var door = door_scene.instance()
+		door.position = Vector2(door_x, 0) * cell_size
+		main_tilemap.set_cell(door_x, -1, -1) # Remove border walls
+		main_tilemap.set_cell(door_x-1, -1, -1)
+		main_tilemap.set_cell(door_x, 0, 7, false, false, false, Vector2(1, 0)) # Floor under door
+		main_tilemap.set_cell(door_x-1, 0, 7, false, false, false, Vector2(1, 0))
+		room.get_node("Doors").add_child(door)
+		room.get_node("DoorTrigger").position = door.position + Vector2(0, cell_size * 1.5)
 	
 	# Generate entrance position
 	var entrance = Position2D.new()
@@ -81,7 +86,7 @@ func generate_room(room_size: Vector2, is_start_room: bool = false, is_end_room:
 	entrance.position = Vector2(entrance_x, entrance_y) * cell_size
 	entrance.name = "Position2D"
 	# Remove wall
-	if not is_start_room:
+	if not start_room:
 		bottom_tilemap.set_cell(entrance_x, room_size.y-1, -1)
 		bottom_tilemap.set_cell(entrance_x-1, room_size.y-1, -1)
 	room.get_node("Entrance").add_child(entrance)
@@ -98,15 +103,15 @@ func generate_room(room_size: Vector2, is_start_room: bool = false, is_end_room:
 
 
 	# Spawn player starting position if starting room
-	if is_start_room:
+	if start_room:
 		var start_x = 1 + randi() % int(room_size.x - 1)
 		var start_y = room_size.y - 2
 		var player_start = Position2D.new()
 		player_start.position = Vector2(start_x, start_y) * cell_size
 		room.get_node("PlayerStart").add_child(player_start)
-		
 		player.position = player_start.position
-	
+
+		
 	return room
 
 
