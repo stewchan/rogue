a4a22fe5ac803e44cbe77a0f0752b914fd6f2159
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-20 18:41:49 -0500

    Goblin throws knife

--- characters/enemy/Goblin.gd
+++ characters/enemy/Goblin.gd
@@ -1,9 +1,15 @@
 extends Enemy
 
+const ThrowableKnifeScene: PackedScene = preload("res://characters/enemy/ThrowableKnife.tscn")
+
 const MAX_DISTANCE_TO_PLAYER: int = 90
 const MIN_DISTANCE_TO_PLAYER: int = 40
 
+export(int) var projectile_speed: int = 150
 
+onready var attack_timer = $AttackTimer
+
+var can_attack: bool = true
 var distance_to_player: float
 
 
@@ -18,9 +24,14 @@ func chase() -> void:
 		if distance_to_player > MAX_DISTANCE_TO_PLAYER:
 			nav_target = player.position
 			nav_agent.set_target_location(nav_target)
-		if distance_to_player < MIN_DISTANCE_TO_PLAYER:
+		elif distance_to_player < MIN_DISTANCE_TO_PLAYER:
 			nav_target = _get_retreat_target()
 			nav_agent.set_target_location(nav_target)
+		else:
+			if can_attack:
+				can_attack = false
+				_throw_knife()
+				attack_timer.start()
 		animated_sprite.flip_h = move_direction.x < 0
 
 
@@ -28,3 +39,13 @@ func _get_retreat_target() -> Vector2:
 	var dir: Vector2 = (global_position - player.global_position).normalized()
 	var target: Vector2 = (global_position + dir * 100)
 	return target
+
+
+func _throw_knife() -> void:
+	var projectile: Area2D = ThrowableKnifeScene.instance()
+	projectile.launch(global_position, (player.position - global_position).normalized(), projectile_speed)
+	get_tree().current_scene.add_child(projectile)
+
+
+func _on_AttackTimer_timeout() -> void:
+	can_attack = true
